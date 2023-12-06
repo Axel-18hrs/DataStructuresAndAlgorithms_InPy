26c89448e6c707d36e64164337464c4fe19aec0c
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-12-06 10:15:02 -0600

    .

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from Classes.Operations.OperationsList import OperationsList
 from Classes.Operations.OperationsStack import OperationsStack
+from Classes.Operations.OperationsQueue import OperationsQueue
 
 
 if __name__ == '__main__':
-    df = OperationsStack()
-    df.menu_stack()
-    ds = OperationsList()
-    ds.menu_list()
+    df = OperationsQueue()
+    df.menu_queue()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
